9e996fa68a7bc7d958ec05d688e407932fd47d93
MerajCodes <113118915+merajCodes3440@users.noreply.github.com>
2023-06-18 17:24:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const removeButton = document.querySelector('input[type="button"]');
+  const colorSelect = document.getElementById("colorSelect");
+
+  removeButton.addEventListener("click", function() {
+    const selectedIndex = colorSelect.selectedIndex;
+    if (selectedIndex !== -1) {
+      colorSelect.remove(selectedIndex);
+    }
+  });
+});
